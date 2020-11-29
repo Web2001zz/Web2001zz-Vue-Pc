ebd23a0181d2813921088f7ce0acee0a9c06186b
Web2001zz <1071358158@qq.com>
2020-11-29 21:28:52 +0800

    feat:Home页面完善

--- src/view/Home/Brand/Brand.vue
+++ src/view/Home/Brand/Brand.vue
@@ -5,33 +5,6 @@
         <li class="brand-item">
           <img src="./images/brand_21.png" />
         </li>
-        <li class="brand-item">
-          <img src="./images/brand_03.png" />
-        </li>
-        <li class="brand-item">
-          <img src="./images/brand_05.png" />
-        </li>
-        <li class="brand-item">
-          <img src="./images/brand_07.png" />
-        </li>
-        <li class="brand-item">
-          <img src="./images/brand_09.png" />
-        </li>
-        <li class="brand-item">
-          <img src="./images/brand_11.png" />
-        </li>
-        <li class="brand-item">
-          <img src="./images/brand_13.png" />
-        </li>
-        <li class="brand-item">
-          <img src="./images/brand_15.png" />
-        </li>
-        <li class="brand-item">
-          <img src="./images/brand_17.png" />
-        </li>
-        <li class="brand-item">
-          <img src="./images/brand_19.png" />
-        </li>
       </ul>
     </div>
   </div>
