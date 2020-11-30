c197ee16d4345a082aaf1a517f6ffaf35f79bf12
Web2001zz <1071358158@qq.com>
2020-11-30 19:48:38 +0800

    feat:登录界面75%

--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -1,7 +1,25 @@
 <template>
-  <div>
-    Login...
-    <el-button type="primary" @click="login">登录</el-button>
+  <div class="Login">
+    <div class="LoginBg">
+      <div class="Login-Body">
+        <div class="Login-Form">
+          <div class="Login-TAB">
+            <h2>扫码登录</h2>
+            <h2>账户登录</h2>
+          </div>
+          <div class="Login-Login">
+            <input type="text" placeholder="输入您的手机号" v-model="user" />
+            <input type="password" placeholder="输入您的密码" />
+          </div>
+          <div class="Login-bottom">
+            <input type="checkout" />自动登录
+            <a href="####">忘记密码？</a>
+            <el-button type="primary" @click="login">登录</el-button>
+            <a href="#">立即注册</a>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -26,4 +44,64 @@ export default {
 </script>
 
 <style lang="less" scoped>
+.Login {
+  width: 100%;
+  height: 500px;
+  /* border: 1px solid black; */
+  background-color: rgb(233, 56, 84);
+  .LoginBg {
+    width: 1100px;
+    margin: 0 auto;
+    height: 100%;
+    background-image: url(../../images/loginbg.png);
+    background-repeat: no-repeat;
+    position: relative;
+  }
+}
+.Login-Body {
+  position: absolute;
+  right: 0;
+  top: 50%;
+  background-color: #fff;
+  width: 400px;
+  height: 400px;
+  margin-top: -200px;
+  display: flex;
+  align-items: center;
+}
+.Login-Form {
+  border: 1px solid black;
+  height: 90%;
+  width: 90%;
+  margin: 0 auto;
+}
+.Login-TAB {
+  display: flex;
+  h2 {
+    width: 50%;
+    height: 40px;
+    line-height: 40px;
+    text-align: center;
+  }
+}
+.Login-Login {
+  margin: 30px 0;
+  input {
+    width: 75%;
+    height: 24px;
+    display: block;
+    margin: 10px auto;
+    text-align: center;
+  }
+}
+.Login-bottom{
+  width: 75% ;
+  margin: 0 auto;
+}
+button {
+  width: 75%;
+  background-color: rgb(233, 56, 84);
+  display: block;
+  margin: 30px auto;
+}
 </style>
